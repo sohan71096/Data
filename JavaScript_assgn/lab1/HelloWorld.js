8033d57c712276300a809cb03840d63f1a60c681
sohan71096 <45695633+sohan71096@users.noreply.github.com>
2018-12-26 18:03:40 +0530

    Add files via upload

---
+++ JavaScript_assgn/lab1/HelloWorld.js
@@ -0,0 +1,4 @@
+function Hello()
+{
+document.write("This text is displayed by Calling External function:<b> Hello World</b>");
+}
---
+++ JavaScript_assgn/lab1/common.js
@@ -0,0 +1,12 @@
+var msg;
+msg="<p><code>The actual script is in external script file called common.js</code></p>"; 
+function addNos(headVar,bodyVar) 
+{
+var result;
+result=headVar+bodyVar;
+document.write("The actual script is in external script file called 'common.js'");
+
+document.write("<br>The sum of the variables headVar and bodyVar is "+result);
+
+
+}
